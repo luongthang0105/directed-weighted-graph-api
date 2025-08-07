f3416d06ea9b37b597ba51e9c301e985b0114b76
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2025-08-07 18:06:31 +1000

    test: constructors tests

--- src/gdwg_graph.test.cpp
+++ src/gdwg_graph.test.cpp
@@ -1,8 +1,55 @@
 #include "gdwg_graph.h"
 
 #include <catch2/catch.hpp>
+#include <vector>
 
-TEST_CASE("Constructors") {}
+TEST_CASE("Constructors") {
+	SECTION("Default constructor") {
+		auto g = gdwg::Graph<int, std::string>{};
+		CHECK(g.empty());
+	}
+	SECTION("InputIt Constructor") {
+		auto nodes = std::vector<int>{1, 2, 3};
+		auto g = gdwg::Graph<int, std::string>{nodes.begin(), nodes.end()};
+		CHECK(g.is_node(1));
+		CHECK(g.is_node(2));
+		CHECK(g.is_node(3));
+	}
+	SECTION("Intializer List Constructor") {
+		auto g = gdwg::Graph<int, std::string>{{1, 2, 3}};
+		CHECK(g.is_node(1));
+		CHECK(g.is_node(2));
+		CHECK(g.is_node(3));
+	}
+	SECTION("Move Constructor") {
+		auto g = gdwg::Graph<int, std::string>{{1, 2, 3}};
+		auto g2 = std::move(g);
+
+		CHECK(g.empty());
+
+		CHECK(g2.is_node(1));
+		CHECK(g2.is_node(2));
+		CHECK(g2.is_node(3));
+
+		// TODO: check iterator created before move is NOT invalidated.
+	}
+	SECTION("Move Assignment") {
+		auto g = gdwg::Graph<int, std::string>{{1, 2, 3}};
+		auto g2 = gdwg::Graph<int, std::string>{{4, 5, 6}};
+		g2 = std::move(g);
+
+		CHECK(g.empty());
+
+		CHECK(g2.is_node(1));
+		CHECK(g2.is_node(2));
+		CHECK(g2.is_node(3));
+
+		CHECK(!g2.is_node(4));
+		CHECK(!g2.is_node(5));
+		CHECK(!g2.is_node(6));
+		// TODO: check iterator created before move is NOT invalidated.
+	}
+}
 
 TEST_CASE("Modifiers") {
 	SECTION("insert_node") {
