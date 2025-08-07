7c1a8f352d40b0ce30e87746dc7baeba1097fca7
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2025-08-07 17:19:36 +1000

    feat: remove impl in .cpp

--- src/gdwg_graph.cpp
+++ src/gdwg_graph.cpp
@@ -1,53 +1,53 @@
 #include "gdwg_graph.h"
 #include <utility>
 
-namespace gdwg {
-	template<typename N, typename E>
-	Graph<N, E>::Graph()
-	: nodes_{}
-	, adjacency_list_{} {}
+// namespace gdwg {
+// 	template<typename N, typename E>
+// 	Graph<N, E>::Graph()
+// 	: nodes_{}
+// 	, adjacency_list_{} {}
 
-	template<typename N, typename E>
-	Graph<N, E>::Graph(std::initializer_list<N> il)
-	: Graph(il.begin(), il.end()) {}
+// 	template<typename N, typename E>
+// 	Graph<N, E>::Graph(std::initializer_list<N> il)
+// 	: Graph(il.begin(), il.end()) {}
 
-	template<typename N, typename E>
-	template<typename InputIt>
-	Graph<N, E>::Graph(InputIt first, InputIt last) {
-		for (auto it = first; it != last; ++it) {
-			insert_node(*it);
-		}
-	}
+// 	template<typename N, typename E>
+// 	template<typename InputIt>
+// 	Graph<N, E>::Graph(InputIt first, InputIt last) {
+// 		for (auto it = first; it != last; ++it) {
+// 			insert_node(*it);
+// 		}
+// 	}
 
-	template<typename N, typename E>
-	Graph<N, E>::Graph(Graph&& other) noexcept
-	: nodes_(std::exchange(other.nodes_, {}))
-	, adjacency_list_(std::exchange(other.adjacency_list_, {})) {}
+// 	template<typename N, typename E>
+// 	Graph<N, E>::Graph(Graph&& other) noexcept
+// 	: nodes_(std::exchange(other.nodes_, {}))
+// 	, adjacency_list_(std::exchange(other.adjacency_list_, {})) {}
 
-	template<typename N, typename E>
-	auto Graph<N, E>::operator=(Graph&& other) noexcept -> Graph& {
-		if (this == &other) {
-			return *this;
-		}
+// 	template<typename N, typename E>
+// 	auto Graph<N, E>::operator=(Graph&& other) noexcept -> Graph& {
+// 		if (this == &other) {
+// 			return *this;
+// 		}
 
-		std::swap(nodes_, other.nodes_);
-		std::swap(adjacency_list_, other.adjacency_list_);
+// 		std::swap(nodes_, other.nodes_);
+// 		std::swap(adjacency_list_, other.adjacency_list_);
 
-		other.adjacency_list_.clear();
-		other.nodes_.clear();
+// 		other.adjacency_list_.clear();
+// 		other.nodes_.clear();
 
-		return *this;
-	}
+// 		return *this;
+// 	}
 
-	template<typename N, typename E>
-	Graph<N, E>::Graph(Graph const& other) {
-		for (auto& [node_ptr, edges] : other.adjacency_list_) {
-			auto clone_edge_set = std::set<std::unique_ptr<Edge<N, E>>>{};
-			auto clone_node = std::make_unique<N>(*node_ptr);
+// 	template<typename N, typename E>
+// 	Graph<N, E>::Graph(Graph const& other) {
+// 		for (auto& [node_ptr, edges] : other.adjacency_list_) {
+// 			auto clone_edge_set = std::set<std::unique_ptr<Edge<N, E>>>{};
+// 			auto clone_node = std::make_unique<N>(*node_ptr);
 
-			for (auto& edge : edges) {
-				clone_edge_set.insert(*edge);
-			}
+// 			for (auto& edge : edges) {
+// 				clone_edge_set.insert(*edge);
+// 			}
 
 			adjacency_list_.insert(std::make_pair(clone_node.get(), clone_edge_set));
 			nodes_.insert(std::move(clone_node));
